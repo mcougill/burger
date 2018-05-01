01f8724e5d175745ee9cb628cf8a89227fd8a27a
Molly Cougill <molly.cougill@gmail.com>
2018-04-30 20:05:21 -0500

    add content to model and controller files

--- config/orm.js
+++ config/orm.js
@@ -2,22 +2,32 @@ var connection = require("./connection.js");
 
 var orm = {
 
+    // selectAll()
+    selectAll: function (whatToSelect, cb) {
+        var queryString = "SELECT * FROM ??";
+        connection.query(queryString, [whatToSelect], function (err, result) {
+            if (err) throw err;
+            cb(result)
+        });
+    },
 
+    // insertOne()
+    insertOne: function (tableInput, values, cb) {
+        var queryString = "INSERT INTO burgers ?? VALUES ?";
+        connection.query(queryString, [tableInput, values], function (err, result) {
+            if (err) throw err;
+            cb(result);
+        })
+    },
 
-
-// selectAll()
-selectAll: function(tableInput, cb){
-    var queryString = "SELECT * FROM ??";
-    connection.query(queryString, [tableInput], function(err, result){
-        if (err) throw err;
-        cb(result);
-    });
-},
-
-// insertOne()
-
-
-// updateOne()
+    // updateOne()
+    updateOne: function (updateTable, updateCol, newVal, filterCol, filterVal, cb) {
+        var queryString = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
+        connection.query(queryString, [updateTable, updateCol, newVal, filterCol, filterVal], function (err, result) {
+            if (err) throw err;
+            cb(result);
+        })
+    }
 
 
 };
---
+++ controllers/burgers_controllers.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var router = express.Router();
+var burger = require("../models/burger.js");
+
+router.get("/", function(req,res){
+    res.redirect("/burgers");
+});
+
+router.get("/burgers", function(req, res){
+    burger.selectAll(function (data){
+        var hbsObj = {burgers:data};
+        console.log(hbsObj);
+        res.render("index", hbsObj);
+    });
+});
+
+router.post("/burgers/insertOne", function(req,res){
+    burger.insertOne(["burger_name","devoured"], [req.body.burger_name, req.body.devoured], function(){
+        res.redirect("/burgers");
+    });
+});
+
+router.put("/burgers/updateOne/:id", function(req, res){
+    var condition = "id = " + req.params.id;
+    console.log("condition: ", condition);
+    burger.updateOne({devoured: req.body.devoured}, condition, function(){
+       res.redirect("/burgers");
+    });
+});
+
+module.exports = router;
